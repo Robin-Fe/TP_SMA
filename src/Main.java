af4596bbde2d9f8679da7e15fd7ae790559c2ded
charlotte <charlotte.finance@etu.univ-lyon1.fr>
2021-11-13 14:19:14 +0100

    ⚡️  Last Update : 100 agents

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
         int deplacementsRandomScore = 0;
         int deplacementsCommunicationScore = 0;
         int deplacementsGlobalScore = 0;
         int actionRandomScore = 0;
         int actionCommunicationScore = 0;
         int actionGlobalScore = 0;
+
         int nbSimulations = 500;
         boolean verbose = false;
         boolean isRandom = true;
-        int nbAgents = 20;
-        int nbPiles = 10;
+        int nbAgents = 10;
+        int nbPiles = 3;
+
         RandomStrategy randomStrategy = new RandomStrategy();
         CommunicateStrategy communicateStrategy = new CommunicateStrategy();
         GlobalStrategy globalStrategy = new GlobalStrategy();
-        for (int i = 0; i < nbSimulations; i++) { ;
+
+        for (int i = 0; i < nbSimulations; i++) {
             System.out.println(i);
             Simulation randomSimulation = new Simulation(verbose, isRandom, randomStrategy, nbAgents, nbPiles);
             randomSimulation.runSimulation();
             deplacementsRandomScore += randomSimulation.getDeplacementScore();
             actionRandomScore += randomSimulation.getActionScore();
+
 
             Simulation communicateSimulation = new Simulation(verbose, isRandom, communicateStrategy, nbAgents, nbPiles);
             communicateSimulation.runSimulation();
@@ -34,13 +40,18 @@ public class Main {
             actionGlobalScore += globalSimulation.getActionScore();
 
         }
+
+
         System.out.println("Pour " + nbSimulations + " Simulations : \n");
-        System.out.println("Deplacements - Score moyen random : " + deplacementsRandomScore/nbSimulations );
-        System.out.println("Deplacements - Score moyen communication : " + deplacementsCommunicationScore/nbSimulations );
-        System.out.println("Deplacements - Score moyen global : " + deplacementsGlobalScore/nbSimulations );
+        System.out.println("Deplacements - Score moyen random : " + deplacementsRandomScore / nbSimulations);
+        System.out.println("Deplacements - Score moyen communication : " + deplacementsCommunicationScore / nbSimulations);
+        System.out.println("Deplacements - Score moyen global : " + deplacementsGlobalScore / nbSimulations);
         System.out.println("\n");
-        System.out.println("Actions - Score moyen random : " + actionRandomScore/nbSimulations );
-        System.out.println("Actions - Score moyen communication : " + actionCommunicationScore/nbSimulations );
-        System.out.println("Actions - Score moyen global : " + actionGlobalScore/nbSimulations );
+        System.out.println("Actions - Score moyen random : " + actionRandomScore / nbSimulations);
+        System.out.println("Actions - Score moyen communication : " + actionCommunicationScore / nbSimulations);
+        System.out.println("Actions - Score moyen global : " + actionGlobalScore / nbSimulations);
+        System.out.println("\n");
+        long stopTime = System.nanoTime();
+        System.out.println("TEMPS TOTAL " + (stopTime - startTime));
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -17,33 +18,20 @@ public class Simulation {
         Objet table = new Objet("Table", null);
         Environment environment = new Environment(nbPiles, table, nbAgents, verbose);
         Agent A = new Agent("A", table);
-        Agent B = new Agent("B", A);
-        Agent C = new Agent("C", B);
-        Agent D = new Agent("D", C);
-        Agent E = new Agent("E", D);
-        Agent F = new Agent("F", E);
-        Agent G = new Agent("G", F);
-        Agent H = new Agent("H", G);
-        Agent I = new Agent("I", H);
-        Agent J = new Agent("J", I);
-        Agent K = new Agent("K", J);
-        Agent L = new Agent("L", K);
-        Agent M = new Agent("M", L);
-        Agent N = new Agent("N", M);
-        Agent O = new Agent("O", N);
-        Agent P = new Agent("P", O);
-        Agent Q = new Agent("Q", P);
-        Agent R = new Agent("R", Q);
-        Agent S = new Agent("S", R);
-        Agent T = new Agent("T", S);
-        Agent U = new Agent("U", T);
-        Agent V = new Agent("V", U);
-        Agent W = new Agent("W", V);
-        Agent X = new Agent("X", W);
-        Agent Y = new Agent("Y", X);
-        Agent Z = new Agent("Z", Y);
-        List<Agent> agents = Arrays.asList(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
-        agents = agents.subList(0, nbAgents);
+        List<Agent> agents = new ArrayList<>();
+        agents.add(A);
+        Agent objective = A;
+        String[] alphabet = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+                "A1","B1","C1","D1","E1", "F1","G1","H1","I1","J1","K1","L1","M1","N1","O1","P1","Q1","R1","S1","T1","U1","V1","W1","X1","Y1","Z1",
+                "A2","B2","C2","D2","E2", "F2","G2","H2","I2","J2","K2","L2","M2","N2","O2","P2","Q2","R2","S2","T2","U2","V2","W2","X2","Y2","Z2",
+                "A3","B3","C3","D3","E3", "F3","G3","H3","I3","J3","K3","L3","M3","N3","O3","P3","Q3","R3","S3","T3","U3","V3","W3","X3","Y3","Z3",
+        };
+
+        for (int i=1; i<nbAgents; i++){
+            Agent newAgent = new Agent(alphabet[i], objective);
+            agents.add(newAgent);
+            objective = newAgent;
+        }
         if (randomOrdering) {
             int r1 = new Random().nextInt(agents.size());
             for (int i = 0; i < agents.size(); i++) {
